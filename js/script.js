fffe9f43e1bede652ea4d41945711c022474b5f1
stefan <stefankaraferovic@gmail.com>
2020-07-20 11:40:55 +0200

    Added win and draw check

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 const gameBoard = (() => {
-    const board = document.querySelector('#board');
-    let fields = [];
+    const board = document.querySelector('.board');
+    let fields = []; // polja na tabli
+    const WINNING_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7], 
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
     
     const startUp = () => {
+        board.classList.add('x');
         for(let i = 0; i < 9; i++){
             fields[i] = '';
             const field = document.createElement('div');
             field.classList.add("field");
             field.setAttribute('data-index', i);
-            field.addEventListener('click', displayController.populateField);
+            field.addEventListener('click', displayController.populateField, {once : true});
             field.textContent = '';
             board.appendChild(field);
         }
@@ -33,18 +44,41 @@ const gameBoard = (() => {
     }
     return{
         startUp,
+        board,
+        fields,
+        WINNING_COMBINATIONS,
     };
 })();
 
 const displayController = (() =>{
     const populateField = () => {
-       if(event.target.textContent == ''){
+       if(!event.target.classList.contains('x') || !event.target.classList.contains('o')){
             if(game.turn == 1){
-                event.target.textContent = '×';
+                gameBoard.board.classList.toggle('x');
+                gameBoard.board.classList.toggle('o');
+                event.target.classList.add('x');
+                gameBoard.fields[event.target.dataset.index] = 'x';
+                if(game.check3InARow('x')){
+                    console.log("Winner x!");
+                    messageShow.win('X');
+                    return;
+                }
                 game.turn = 2;
             }else{
-                event.target.textContent = '○';
+                gameBoard.board.classList.toggle('x');
+                gameBoard.board.classList.toggle('o');
+                event.target.classList.add('o');
+                gameBoard.fields[event.target.dataset.index] = 'o';
+                if(game.check3InARow('o')){
+                    console.log("Winner o!");
+                    messageShow.win("O");
+                    return;
+                }
                 game.turn = 1;
+            }
+            console.table(gameBoard.fields);
+            if(game.checkDraw()){
+                console.log("Its draw!")
             }
        }
     }
@@ -55,14 +89,39 @@ const displayController = (() =>{
 
 const game = (() => {
     let turn = 1;
-    const check3InARow = () => {
-       //for(let i = )
+    const check3InARow = (sign) => {
+       return gameBoard.WINNING_COMBINATIONS.some(rowOrCol => {
+           return rowOrCol.every(field => {
+               return gameBoard.fields[field] == sign;
+           })
+       })
 
+    }
+    const checkDraw = () => {
+        return gameBoard.fields.every(field => {
+            return field != '';
+        })
     }
 
     return{
         turn,
         check3InARow,
+        checkDraw,
+    }
+})();
+
+const messageShow = (() => {
+    const win = (sign) => {
+
+    };
+
+    const draw = () => {
+
+    }
+    return{
+        win,
+        draw,
+        
     }
 })();
 
@@ -76,7 +135,7 @@ const player = (name, sign) => {
 };
 
 const player1 = player('Stefan', 'x');
-const player2 = player('Coka', 'o');
+const player2 = player('Aleksa', 'o');
 
 
 
